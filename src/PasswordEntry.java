9dbfbf0444973d0d03b1aa9c7bc98fc3eff0fce9
JJ <joshua.james918@gmail.com>
2025-07-31 18:54:45 -0500

    formatted and passwordentry return

--- src/PasswordEntry.java
+++ src/PasswordEntry.java
@@ -3,18 +3,34 @@ public class PasswordEntry {
     private String username;
     private String password;
 
-    public PasswordEntery(String site, String username; String password;) {
+    public PasswordEntry(String site, String username, String password) {
         this.site = site;
         this.site = username;
         this.site = password;
     }
 
-    public String getSite () {return site; }
-    public String getUsername () {return username; }
-    public Sting getPassword () {return password; }
+    public String getSite () { 
+        return site; 
+    }
+
+    public String getUsername () { 
+        return username; 
+    }
+
+    public String getPassword () { 
+        return password; 
+    }
+
+    public static PasswordEntry fromString(String line) {
+        String[] parts = line.split(",", 3);
+        if (parts.length == 3) {
+            return new PasswordEntry(parts[0], parts[1], part[2]);
+        }
+        return null;
+    }
 
     @Override
     public String toString () {
-        return site + "," + username, "," + password;
+        return site + "," + username + "," + password;
     }
 }
