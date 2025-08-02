5ad305909903e71ccf4c30ff4a7a88f431832899
JJ <joshua.james918@gmail.com>
2025-08-02 01:11:49 -0500

    formatting on password entry and added encrptionlogic

--- src/PasswordEntry.java
+++ src/PasswordEntry.java
@@ -5,8 +5,8 @@ public class PasswordEntry {
 
     public PasswordEntry(String site, String username, String password) {
         this.site = site;
-        this.site = username;
-        this.site = password;
+        this.username = username;
+        this.password = password;
     }
 
     public String getSite () { 
@@ -21,16 +21,20 @@ public class PasswordEntry {
         return password; 
     }
 
+   // Converts a CSV line into a PasswordEntry, decrypting the password
     public static PasswordEntry fromString(String line) {
         String[] parts = line.split(",", 3);
         if (parts.length == 3) {
-            return new PasswordEntry(parts[0], parts[1], part[2]);
+            String decryptedPassword = EncryptionUtils.decrypt(parts[2]);
+            return new PasswordEntry(parts[0], parts[1], decryptedPassword);
         }
         return null;
     }
 
+    // Converts the entry into a CSV line, encrypting the password
     @Override
-    public String toString () {
-        return site + "," + username + "," + password;
+    public String toString() {
+        String encryptedPassword = EncryptionUtils.encrypt(password);
+        return site + "," + username + "," + encryptedPassword;
     }
 }
